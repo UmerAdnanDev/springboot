5a1c3e77f86eaf4ed43d8739bdd717ad8961210c
Muhammad Umer Adnan <omeradnannovember2005@gmail.com>
2026-05-14 22:53:42 +0500

    added spring initializer project
    this is a spring boot template created using spring initializer

---
+++ 1-Basics/1-Basics/src/main/java/com/example/topic1/Application.java
@@ -0,0 +1,14 @@
+package com.example.topic1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+		System.out.println("\n Spring Boot Project Created From Spring Initializer");
+	}
+
+}
