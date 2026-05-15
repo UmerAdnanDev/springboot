0da2907097fbdd24dee28e4e47155ad1cc2c1cf2
Muhammad Umer Adnan <omeradnannovember2005@gmail.com>
2026-05-15 23:36:34 +0500

    updated main class to initiate manual class
    This creates instance of ManualComponent class

--- 1-Basics/1-Basics/src/main/java/com/example/topic1/Application.java
+++ 1-Basics/1-Basics/src/main/java/com/example/topic1/Application.java
@@ -2,13 +2,19 @@ package com.example.topic1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
-@SpringBootApplication
+@SpringBootApplication //this annotation sets up everything automatically and finds all components  
 public class Application {
-
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-		System.out.println("\n Spring Boot Project Created From Spring Initializer");
+		// SpringApplication.run(Application.class, args);
+		System.out.println("\n Spring Boot Project Created From Spring Initializer\n");
+		// This starts Spring's container
+        ApplicationContext context = SpringApplication.run(Application.class, args);
+
+        // WAY 1: MANUAL - You create the object yourself
+        ManualComponent mycomp = new ManualComponent();
+        mycomp.Msg();
 	}
 
 }
