6ffbe902df99c8680e4d9ae8eb354ee2e093538b
Muhammad Umer Adnan <omeradnannovember2005@gmail.com>
2026-05-17 00:44:34 +0500

    added Product Entity class

---
+++ 2-RestApi-h2-DB/2-RestApi-h2-DB/src/main/java/com/example/restapih2/Entity/Product.java
@@ -0,0 +1,54 @@
+package com.example.restapih2.Entity;
+import jakarta.persistence.*;
+@Entity
+@Table(name = "product")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Auto increments id 
+    private Long id;
+    private String name;
+    private String description;
+    private Double price;
+    
+    // Constructors
+    public Product() {}
+    
+    public Product(String name, String description, Double price) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+    
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+    
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public String getDescription() {
+        return description;
+    }
+    
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+    public Double getPrice() {
+        return price;
+    }
+    
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+}
