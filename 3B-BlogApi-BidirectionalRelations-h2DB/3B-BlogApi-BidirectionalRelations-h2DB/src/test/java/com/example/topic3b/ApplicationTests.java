68972a76babb31ed69e68f50b13a9aa5aaf69c7c
Muhammad Umer Adnan <omeradnannovember2005@gmail.com>
2026-05-24 02:30:01 +0500

    added boilerplate of 3B-BlogApi-BidirectionalRelations-h2DB

---
+++ 3B-BlogApi-BidirectionalRelations-h2DB/3B-BlogApi-BidirectionalRelations-h2DB/src/test/java/com/example/topic3b/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.topic3b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
