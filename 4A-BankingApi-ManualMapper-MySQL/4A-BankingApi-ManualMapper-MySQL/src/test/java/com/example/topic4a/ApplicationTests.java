0b6a8ddc0ef0e484f6fe5ea114c7b3db33b06902
Muhammad Umer Adnan <omeradnannovember2005@gmail.com>
2026-05-26 22:34:06 +0500

    added boilerplate for Banking Api
    This project will demonstrates DTO , Manual mapper , Validation and Lombok implementation

---
+++ 4A-BankingApi-ManualMapper-MySQL/4A-BankingApi-ManualMapper-MySQL/src/test/java/com/example/topic4a/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.topic4a;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
