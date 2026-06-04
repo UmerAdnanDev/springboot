2f9441c9ac288d7c85a3942dd3f2fa08c6cd08c1
Muhammad Umer Adnan <omeradnannovember2005@gmail.com>
2026-06-04 18:53:06 +0500

    added project description  in main class as string

--- 4A-BankingApi-ManualMapper-MySQL/4A-BankingApi-ManualMapper-MySQL/src/main/java/com/example/topic4a/Application.java
+++ 4A-BankingApi-ManualMapper-MySQL/4A-BankingApi-ManualMapper-MySQL/src/main/java/com/example/topic4a/Application.java
@@ -8,6 +8,7 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+		System.out.println("Banking API demonsterating use of data tranfer object layer to remove over fetching of data and improving security along with performance,mannual mapper layer a pipeline between entity and dto , used validation dependency for edge cases,loombok for reducing boileplate code and MySQl for database ");
 	}
 
 }
